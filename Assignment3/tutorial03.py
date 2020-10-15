876e019e7854d1e76bcca52de327365e7df42588
vkk_17 <56954990+vkk1710@users.noreply.github.com>
2020-10-15 17:02:00 +0530

    modified the code of course task by removing existing folders

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -5,6 +5,8 @@ import shutil
 
 def course():
     # Read csv and process
+    shutil.rmtree('analytics\course')
+    os.mkdir('analytics\course')
     with open('studentinfo_cs384.csv','r') as file :
         dict_reader = csv.DictReader(file)
         field = ['id','full_name','country','email','gender','dob','blood_group','state']
@@ -156,4 +158,4 @@ def new_file_sort():
             writer = csv.DictWriter(f, fieldnames = field)
             writer.writerow({'id':row['id'],'first_name':row['first_name'],'last_name':row['last_name'],'country':row['country'],'email':row['email'],'gender':row['gender'],'dob':row['dob'],'blood_group':row['blood_group'],'state':row['state']})    
         
-new_file_sort()
+course()
