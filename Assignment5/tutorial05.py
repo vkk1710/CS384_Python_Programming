c10ea46bd8914176a0f98ec0905973bfd715c917
vkk_17 <56954990+vkk1710@users.noreply.github.com>
2020-11-16 17:37:37 +0530

    Added the code for renaming FIR files

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -1,8 +1,37 @@
 import os
 import re
 
+os.chdir('F:/Acads/5th sem/Python CS384/CS384_1801CE31/Assignment5')
 def rename_FIR(folder_name):
-    # rename Logic
+    print('Enter Season number padding : ')
+    season_pad = int(input())
+    print('Enter Episode number padding : ')
+    episode_pad = int(input())
+    
+    i=0
+    old_title=[]
+    new_title=[]
+    extension_list=[]
+    for f in os.scandir('Subtitles/'+folder_name):
+        if(f.is_file()):
+            split=re.findall(r'\d+',f.name)
+            episode_num = str(split[0])
+            extension = f.name[-4:]
+            extension_list.append(extension)
+            if(episode_pad>len(episode_num)):
+                for i in range(episode_pad-len(episode_num)):
+                    episode_num = '0' + episode_num
+            new_name = folder_name + ' - ' + 'Episode ' + episode_num
+            old_title.append(f.name)
+            new_title.append(new_name)
+    for i in zip(old_title,new_title,extension_list):
+        new_name = i[1]
+        old_name = i[0]
+        extension = i[2]
+        if(os.path.exists('Subtitles/'+folder_name+'/'+new_name+extension)):
+            os.rename('Subtitles/'+folder_name+'/'+old_name, 'Subtitles/'+folder_name+'/'+new_name+'_copy'+extension)
+        else:
+            os.rename('Subtitles/'+folder_name+'/'+old_name, 'Subtitles/'+folder_name+'/'+new_name+extension)            
     pass
     
 
